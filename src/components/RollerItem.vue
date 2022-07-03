6f3aad33d9ec2151f579a29123bad074ccdc1879
andy-0414 <pjh8667@naver.com>
2022-07-03 16:29:55 +0900

    short, long mode

--- src/components/RollerItem.vue
+++ src/components/RollerItem.vue
@@ -1,3 +1,10 @@
+<script lang="ts">
+enum RollerItemMode {
+    SHORT = "short",
+    LONG = "long",
+}
+</script>
+
 <script setup lang="ts">
 import { computed } from "@vue/reactivity";
 import { toRefs } from "vue";
@@ -7,16 +14,21 @@ interface Props {
     char?: string;
     duration?: number;
     charSet?: string[];
+    mode?: RollerItemMode;
 }
 const props = withDefaults(defineProps<Props>(), {
     char: "",
     charSet: () => [...Array(10).keys()].map(String),
     duration: 500,
+    mode: RollerItemMode.SHORT,
 });
 const { char, charSet, duration } = toRefs(props);
 
 const { isReady, isEnd, targetIdx, prevTargetIdx } = useAnimationManager(char, charSet, duration);
 
+/**
+ * @description Now the top value of the roller
+ */
 const top = computed(() => {
     let idx = targetIdx.value;
     if (!isReady.value) {
@@ -26,22 +38,36 @@ const top = computed(() => {
     if (idx == -1) return "25%";
     return `${25 - idx * 50}%`;
 });
+
+/**
+ * @description Array of characters to be displayed after animation ends
+ */
 const shortCharSet = computed(() => {
+    // In case of short mode, it is sufficient to show only one char
+    if (props.mode == RollerItemMode.SHORT) return [props.char];
+
+    // For values not in charSet, an empty string is inserted before and after.
     if (targetIdx.value == -1) return ["", props.char, ""];
+
+    // at the beginning of the array
     if (targetIdx.value == 0) return ["", props.char, props.charSet[targetIdx.value + 1]];
+
+    // at the end of the array
     if (targetIdx.value == charSet.value.length - 1) return [props.charSet[targetIdx.value - 1], props.char, ""];
+
+    // in all other cases
     return props.charSet.slice(targetIdx.value - 1, targetIdx.value + 2);
 });
 </script>
 
 <template>
     <div class="roller-item">
-        <div class="roller-item__wrapper" v-if="isEnd">
+        <div class="roller-item__wrapper" :class="{ 'roller-item__wrapper--short': mode == RollerItemMode.SHORT }" v-if="isEnd">
             <div class="roller-item__wrapper__list">
                 <div class="roller-item__wrapper__list__item" v-for="item of shortCharSet">{{ item }}</div>
             </div>
         </div>
-        <div class="roller-item__wrapper" v-else>
+        <div class="roller-item__wrapper" :class="{ 'roller-item__wrapper--short': mode == RollerItemMode.SHORT }" v-else>
             <div class="roller-item__wrapper__list" :style="{ top, transition: `${duration}ms` }">
                 <div class="roller-item__wrapper__list__item" v-for="item of charSet">{{ item }}</div>
             </div>
@@ -72,10 +98,12 @@ const shortCharSet = computed(() => {
         mask-image: linear-gradient(0deg, rgba(255, 255, 255, 0) 0%, rgba(0, 0, 0, 1) 25%, rgba(0, 0, 0, 1) 75%, rgba(255, 255, 255, 0) 100%);
         -webkit-mask-image: linear-gradient(0deg, rgba(255, 255, 255, 0) 0%, rgba(0, 0, 0, 1) 25%, rgba(0, 0, 0, 1) 75%, rgba(255, 255, 255, 0) 100%);
 
-        /* mask-image: linear-gradient(0deg, rgba(255, 255, 255, 0) 20%, rgba(0, 0, 0, 1) 45%, rgba(0, 0, 0, 1) 55%, rgba(255, 255, 255, 0) 80%);
-        -webkit-mask-image: linear-gradient(0deg, rgba(255, 255, 255, 0) 20%, rgba(0, 0, 0, 1) 45%, rgba(0, 0, 0, 1) 55%, rgba(255, 255, 255, 0) 80%); */
-
         box-sizing: border-box;
+
+        &.roller-item__wrapper--short {
+            mask-image: linear-gradient(0deg, rgba(255, 255, 255, 0) 20%, rgba(0, 0, 0, 1) 45%, rgba(0, 0, 0, 1) 55%, rgba(255, 255, 255, 0) 80%);
+            -webkit-mask-image: linear-gradient(0deg, rgba(255, 255, 255, 0) 20%, rgba(0, 0, 0, 1) 45%, rgba(0, 0, 0, 1) 55%, rgba(255, 255, 255, 0) 80%);
+        }
         .roller-item__wrapper__list {
             position: absolute;
 
