560c62583661a631b14c3dab5af69e7c5089ef02
andy-0414 <pjh8667@naver.com>
2022-07-03 16:36:19 +0900

    useAnimationManager add comment

--- src/composables/useAnimationManager.ts
+++ src/composables/useAnimationManager.ts
@@ -1,11 +1,20 @@
 import { computed } from "@vue/reactivity";
-import { Ref, ref, watch, toRefs } from "vue";
+import { Ref, ref, watch } from "vue";
 
+/**
+ * @description Manages the life cycle of animations that are executed according to changes in char values
+ * @param char This is the target value, and the animation runs when the value changes.
+ * @param charSet A set of target values, rolled according to the order of the list
+ * @param duration animation duration
+ */
 export default function useAnimationManager(char: Ref<string>, charSet: Ref<string[]>, duration: Ref<number>) {
-    const isReady = ref(false);
-    const isEnd = ref(false);
-    const prevTargetIdx = ref(0);
+    const isReady = ref(false); // Ready to run animation (default 100ms)
+    const isEnd = ref(false); // End all animations
 
+    const targetIdx = computed(() => charSet.value.indexOf(char.value)); // Target index in charSet
+    const prevTargetIdx = ref(0); // Target index in charSet just before
+
+    // An animation start function that runs when a value changes
     function reloadAnimation() {
         isReady.value = false;
         isEnd.value = false;
@@ -17,6 +26,7 @@ export default function useAnimationManager(char: Ref<string>, charSet: Ref<stri
         }, 100);
     }
 
+    // Check char value change
     const w = watch(
         () => char.value,
         (next, prev) => {
@@ -25,8 +35,7 @@ export default function useAnimationManager(char: Ref<string>, charSet: Ref<stri
         }
     );
 
-    const targetIdx = computed(() => charSet.value.indexOf(char.value));
-
     reloadAnimation();
+
     return { isReady, isEnd, targetIdx, prevTargetIdx };
 }
